0db76f3e90f9b3de52dab9121b0b6fc5a61afb3d
cassiaavila <laercio.aaavila@gmail.com>
2022-04-21 12:52:03 -0300

    Spring initializr
    inicio do projeto
    
    pom.xml

---
+++ src/main/java/br/com/laercio/listacias/listacias/ListaCiasApplication.java
@@ -0,0 +1,13 @@
+package br.com.laercio.listacias.listacias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaCiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaCiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/laercio/listacias/listacias/ListaCiasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laercio.listacias.listacias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaCiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
